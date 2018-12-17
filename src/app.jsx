8b7329c8c6ae7e9b0b854f8296e8bd122f819b55
wxxeibo <wxxeibo@gmail.com>
2018-12-18 00:57:17 +0800

    Add antd

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Input } from 'antd';
+// import 'antd/dist/antd.css';
 
 export default class App extends React.Component {
   render() {
     return (<div>
       <h2>Welcome to React!</h2>
+      <Input />
     </div>);
   }
 }

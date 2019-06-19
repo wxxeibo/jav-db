cc0e09a75c441ca9d5313714a6386ac90986cb05
wxxeibo <wxxeibo@gmail.com>
2019-06-20 01:39:09 +0800

    Read the selected file

--- src/Posters.jsx
+++ src/Posters.jsx
@@ -2,6 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Input, Button, Row, Col } from 'antd';
 import { remote } from 'electron';
+import fs from 'fs';
+
+const readFile = (filePath) => {
+  let content;
+  function processFile() {
+    console.log(content);
+  }
+  // First I want to read the file
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    content = data;
+
+    // Invoke the next step here however you like
+    console.log(content); // Put all of the code here (not the best solution)
+    processFile(); // Or put the next step in a function and invoke it
+  });
+};
 
 const propTypes = {
   value: PropTypes.arrayOf(PropTypes.string), // eslint-disable-line react/require-default-props
@@ -38,8 +57,17 @@ export default class Posters extends React.Component {
      * @param {string[]} [localPaths=[]] Is undefined when use not select a file
      */
     const finishSelectFiles = (localPaths = []) => {
+      console.log(localPaths);
       this.setState({ localPaths });
+      readFile(localPaths[0]);
     };
+    // Notes:
+    // The Dialog is opened from Electron's main thread.
+    // If you want to use the dialog object from a renderer process,
+    // remember to access it using the remote
+    //
+    // For more details about how to use dialog, please see:
+    // https://electronjs.org/docs/api/dialog
     remote.dialog.showOpenDialog({ title: 'find posters' }, finishSelectFiles);
   }
 
